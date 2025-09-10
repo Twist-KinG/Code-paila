3dad66db1edc1a8af13f3ed042972ab6b32444f2
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-09-10 22:14:44 +0545

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Portfolio from "./pages/Portfolio";
+import Team from "./pages/Team";
 import Contact from "./pages/Contact";
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/portfolio" element={<Team />} />
+          <Route path="/team" element={<Portfolio />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
