e8d047f198ae8f7f3cd0a94b4dcc409e1d0666ec
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-09-14 22:50:14 +0545

    Update CareerManagement.jsx

--- client/src/components/Admin/CareerManagement.jsx
+++ client/src/components/Admin/CareerManagement.jsx
@@ -245,12 +245,7 @@ const CareerManagement = () => {
             </select>
 
             <div className="flex justify-end gap-2 flex-wrap">
-              <button
-                type="button"
-                onClick={() => {
-                  setShowForm(false);
-                  setEditId(null);
-                }}
+              <button type="button" onClick={() => { setShowForm(false); setEditId(null);}}
                 className="px-4 py-2 bg-gray-400 text-white rounded-lg">Cancel</button>
               <button type="submit" className="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700">
                 {editId ? "Update" : "Add"}
