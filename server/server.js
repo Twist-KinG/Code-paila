46b23e1d135c721f39158ac37e982ff9eed5fa7d
Dipesh-Theeng-Tamang <dt412242@gmail.com>
2025-12-09 20:12:26 +0545

    all

--- server/server.js
+++ server/server.js
@@ -29,5 +29,7 @@ app.use("/api/admin/career", careerRoutes);
 app.use("/api/admin/blog", blogRoutes);
 app.use("/api/admin/portfolio", portfolioRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
